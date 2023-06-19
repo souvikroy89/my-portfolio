9dec47aaa02bd015ce93e5baa361b56f9dbb5a5d
Souvik Roy <133473759+souvikroy89@users.noreply.github.com>
2023-06-19 15:37:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["STUDENT", "CODER", "DEVELOPER", "DESIGNER"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+    });
